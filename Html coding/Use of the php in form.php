7fe2d63bcc551c550ac67a1f62505c6da22e15fd
harsh-gupta283104 <149651106+harsh-gupta283104@users.noreply.github.com>
2023-11-02 13:18:34 +0530

    Add files via upload

---
+++ Html coding/Use of the php in form.php
@@ -0,0 +1,13 @@
+<?php
+echo
+"<h1>
+There are some usable link
+</h1>
+<ul>
+<li><a href=https://github.com/login>Go to Gihtub</a>
+<li><a href=calculator.html>Go to calculator</a>
+<li><a href=https://www.linkedin.com/feed/>Go to linkedin</a>
+<li><a href=https://cdnjs.com/>Go to cdn</a>
+<li><a href=https://www.youtube.com/>Go to youtube</a>
+</ul>";
+?>
---
+++ Html coding/java.js
@@ -0,0 +1,5 @@
+gsap.to("#ex2",{
+    x:200,
+    duration:2,
+    dilay:1,
+});
